090a18a77abeeb42d9621015914e8305e2ca9c37
ningenpaper <163857391+ningenpaper@users.noreply.github.com>
2024-03-19 22:55:26 +0900

    Delete js directory

--- js/alert.js
+++
@@ -1,9 +0,0 @@
-function alert() {
-  var txt;
-  if (confirm("Under Construction. Will let you know when it's ready.")) {
-    txt = "You pressed OK!";
-  } else {
-    txt = "You pressed Cancel!";
-  }
-  document.getElementById("demo").innerHTML = txt;
-}
